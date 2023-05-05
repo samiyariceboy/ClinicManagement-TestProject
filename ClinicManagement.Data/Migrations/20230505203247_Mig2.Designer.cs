2fe9a0519c6101548aed3c88806cb66d2410a7d2
samiyar ICEBOY <samiyariceboy@gmail.com>
2023-05-06 01:08:26 +0430

    update connection string and add migration

--- ClinicManagement.Data/Migrations/20230505203247_Mig2.Designer.cs
+++ ClinicManagement.Data/Migrations/20230505203247_Mig2.Designer.cs
@@ -4,6 +4,7 @@ using ClinicManagement.Data.DbContexts.Sql.SqlServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ClinicManagement.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230505203247_Mig2")]
+    partial class Mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ClinicManagement.Data/Migrations/20230505203247_Mig2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicManagement.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "AppointmentAggregateRoots",
+                newName: "PatientUserId");
+
+            migrationBuilder.CreateTable(
+                name: "ClinicAggregateRoots",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWSEQUENTIALID()"),
+                    ClinicName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateOfEstablishment = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ClinicAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClinicAggregateRoots", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentAggregateRoots_PatientUserId",
+                table: "AppointmentAggregateRoots",
+                column: "PatientUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AppointmentAggregateRoots_AspNetUsers_PatientUserId",
+                table: "AppointmentAggregateRoots",
+                column: "PatientUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AppointmentAggregateRoots_AspNetUsers_PatientUserId",
+                table: "AppointmentAggregateRoots");
+
+            migrationBuilder.DropTable(
+                name: "ClinicAggregateRoots");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AppointmentAggregateRoots_PatientUserId",
+                table: "AppointmentAggregateRoots");
+
+            migrationBuilder.RenameColumn(
+                name: "PatientUserId",
+                table: "AppointmentAggregateRoots",
+                newName: "UserId");
+        }
+    }
+}
